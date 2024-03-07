a0b034b509152de849d7aeaccc347feaf1491ca6
unknown <ganarez03@gmail.com>
2024-03-07 22:19:37 +0700

    update nav link

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon')
-const navList = document.querySelector('nav ul')
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const navList = document.querySelector("nav ul");
+const navLink = document.querySelectorAll("nav ul li a");
 
-hamburgerIcon.addEventListener('click', () => {
-    navList.classList.toggle('tampil-nav-ul')
-})
+hamburgerIcon.addEventListener("click", () => {
+  navList.classList.toggle("tampil-nav-ul");
+});
+
+navLink.forEach((item) => {
+  item.addEventListener("click", () => {
+    navList.classList.remove("tampil-nav-ul");
+  });
+});
