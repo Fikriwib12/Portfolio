35afadee452b64fc34bb898a7ceeb86e8050c833
Fikriwib12 <wibfikri@gmail.com>
2024-04-12 13:14:04 +0700

    update dark mode

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ navLink.forEach((item) => {
     navList.classList.remove("tampil-nav-ul");
   });
 });
+
+// dark mode
+const darkModeBtn = document.querySelector(".dark-mode");
+darkModeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+  darkModeBtn.classList.toggle("fa-moon");
+  darkModeBtn.classList.toggle("fa-sun");
+  const isDarkmode = document.body.classList.contains("dark");
+  localStorage.setItem("darkMode", isDarkmode);
+});
+
+const toggle = document.getElementById("check");
+
+toggle.addEventListener("change", function () {
+  document.body.classList.toggle("dark");
+  darkModeBtn.classList.toggle("fa-sun");
+  darkModeBtn.classList.toggle("fa-moon");
+  const isDarkMode = document.body.classList.contains("dark");
+  localStorage.setItem("darkMode", isDarkMode);
+});
+const isDarkmode = localStorage.getItem("darkMode") === "true";
+
+if (isDarkmode) {
+  document.body.classList.add("dark");
+  darkModeBtn.classList.toggle("fa-moon");
+  darkModeBtn.classList.toggle("fa-sun");
+}
